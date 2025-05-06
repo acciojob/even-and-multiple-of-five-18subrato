c6920845cb4820726cbcc6ef2cf9e559834bc247
Subrato Kumar Verma <138965935+18subrato@users.noreply.github.com>
2025-05-07 01:14:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 function checkNumber(num) {
-	if(typeof num === 'string'){
-		return false;
-	}
-
 	num = +num;
-	if(num <(-10 ** 9) && num>(10**9)){
-		return false;
-	}
-
+	
 	if(num %2 === 0 && num%5 ===0){
 		return true;
 	} else {
 		return false;
 	}
 	
+	if(typeof num === 'string'){
+		return false;
+	}
+
+	
+	
+
+	
+	
 }
 
-const num = parseInt(prompt("Enter a number."));
+const num = parseInt(prompt("Enter a number."))
 alert(checkNumber(num));
