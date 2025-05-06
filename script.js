afcb190fb357a26a135ac87cb538867d01163c97
Subrato Kumar Verma <138965935+18subrato@users.noreply.github.com>
2025-05-07 01:11:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function checkNumber(num) {
-  //your code here
+	if(typeof num === 'string'){
+		return false;
+	}
+
+	num = +num;
+	if(num <(-10 ** 9) && num>(10**9)){
+		return false;
+	}
+
+	if(num %2 === 0 && num%5 ===0){
+		return true;
+	} else {
+		return false;
+	}
+	
 }
 
 const num = parseInt(prompt("Enter a number."));
